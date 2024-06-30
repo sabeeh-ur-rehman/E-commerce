cbb7d93506e5c5abe974116cdd08d98ff58cfa02
sabeeh21 <onamouceclick@gmail.com>
2024-06-30 14:43:34 +0500

    d

--- src/components/home/Productcard.js
+++ src/components/home/Productcard.js
@@ -94,7 +94,7 @@ const Productcard = ({ startId = 1, endId = 50 }) => {
   }
 
   return (
-    <div className="cards-container grid gap-y-8 items-end grid-cols-5 max-md:grid-cols-3 max-sm:grid-cols-2">
+    <div className="cards-container grid gap-y-8 items-end grid-cols-5 max-md:grid-cols-3 max-sm:grid-cols-1">
       {content}
     </div>
   );
